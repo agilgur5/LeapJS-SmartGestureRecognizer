76fc44e7c2e3fbd93cadc5a21da00fb05c84550e
Anton Gilgur <agilgur5@gmail.com>
2014-12-18 03:30:20 -0500

    kk started using boilerplate hand with some modifications. Only recognizes one hand, but that's good enough for now

--- renderer.js
+++ renderer.js
@@ -1,204 +1,61 @@
-var init = function(){
-  var scene = new THREE.Scene();
-  var container = document.getElementById( 'visualizer' );
-  var camera = new THREE.PerspectiveCamera(45, container.offsetWidth / container.offsetHeight, 0.10, 1000);
-  var renderer = new THREE.WebGLRenderer();
+var renderer, scene, camera, controls, stats, container;
+var info, palm, phalanges = [];
+init();
+animate();
+
+function init() {
+  var light, geometry, material, mesh;
+  // Three.js basics
+  scene = new THREE.Scene();
+  container = document.getElementById( 'visualizer' );
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize( container.offsetWidth, container.offsetHeight );
   container.appendChild( renderer.domElement );
-  camera.position.z = 500;
-  camera.position.y = 200;
-  camera.lookAt(new THREE.Vector3(0,200,0))
-    
-  var fingers = {};
-  var spheres = {};
-
-  Leap.loop(function(frame) {
-
-    var fingerIds = {};
-    var handIds = {};
-
-    for (var index = 0; index < frame.pointables.length; index++) {
-
-      var pointable = frame.pointables[index];
-      var finger = fingers[pointable.id];
-
-      var pos = pointable.tipPosition;
-      var dir = pointable.direction;
-
-      var origin = new THREE.Vector3(pos[0], pos[1], pos[2]);
-      var direction = new THREE.Vector3(dir[0], dir[1], dir[2]);
-
-      if (!finger) {
-        finger = new THREE.ArrowHelper(origin, direction, 40, Math.random() * 0xffffff);
-        fingers[pointable.id] = finger;
-        scene.add(finger);
-      }
-
-      finger.position = origin;
-      finger.setDirection(direction);
-
-      fingerIds[pointable.id] = true;
+  camera = new THREE.PerspectiveCamera( 40, container.offsetWidth / container.offsetHeight, 1, 3000 );
+  camera.position.set( 0, 250, 600 );
+  controls = new THREE.TrackballControls( camera, renderer.domElement );
+  controls.target.set( 0, 100, 0 );
+  // palm
+  geometry = new THREE.BoxGeometry( 80, 20, 80 );
+  geometry.applyMatrix( new THREE.Matrix4().makeTranslation( 0, 0, -30 ) );  // to to +30 if using pitch roll & yaw
+  material = new THREE.MeshNormalMaterial();
+  palm = new THREE.Mesh( geometry, material );
+  scene.add( palm );
+  // phalanges
+  geometry = new THREE.BoxGeometry( 16, 12, 1 );
+  for ( var i = 0; i < 15; i++) {
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+    phalanges.push( mesh );
+  }
+}
+Leap.loop( function( frame ) {
+  var hand, phalanx, point, length;
+  if ( frame.hands.length ) {
+    hand = frame.hands[0];
+    palm.position.set( hand.palmPosition[0], hand.palmPosition[1], hand.palmPosition[2] );
+    //      palm.rotation.set( hand.pitch(), -hand.yaw(), hand.roll() );
+    direction = new THREE.Vector3( hand.direction[0], hand.direction[1], hand.direction[2] );  // best so far
+    palm.lookAt( direction.add( palm.position ) );
+    palm.rotation.z = -hand.roll();
+  }
+  var iLen = ( frame.pointables.length < 5 ) ? frame.pointables.length : 5;
+  for (var i = 0; i < iLen; i++) {
+    for ( var j = 0; j < 3; j++) {
+      phalanx = phalanges[ 3 * i + j];
+      point = frame.pointables[i].positions[j];
+      phalanx.position.set( point[0], point[1], point[2] );
+      point = frame.pointables[i].positions[ j + 1 ];
+      point = new THREE.Vector3( point[0], point[1], point[2] );
+      phalanx.lookAt( point );
+      length = phalanx.position.distanceTo( point );
+      phalanx.translateZ( 0.5 * length );
+      phalanx.scale.set( 1, 1, length );
     }
-
-    for (fingerId in fingers) {
-      if (!fingerIds[fingerId]) {
-        scene.remove(fingers[fingerId]);
-        delete fingers[fingerId];
-      }
-    }
-
-    //if(frame.gestures.length > 0) console.log(frame.gestures);
-
-    renderer.render(scene, camera);
-  });
-};
-setTimeout(init, 100);
-
-  // // Global Variables for THREE.JS
-  // var container , camera, scene, renderer , stats;
-  // // Global variable for leap
-  // var frame, controller;
-
-  // // Setting up how big we want the scene to be
-  // var sceneSize = 100;
-  // var leftHand , rightHand;
-
-
-  // // Get everything set up
-  // init();
-  // // Start the frames rolling
-  // animate();
-
-
-  // function init(){
-
-  //   controller = new Leap.Controller();
-  //   scene = new THREE.Scene();
-  //   container = document.getElementById( 'visualizer' );
-    
-  //   camera = new THREE.PerspectiveCamera( 
-  //     50 ,
-  //     container.offsetWidth / container.offsetHeight,
-  //     sceneSize / 100 ,
-  //     sceneSize * 4
-  //   );
-  //   // placing our camera position so it can see everything
-  //   camera.position.z = sceneSize;
-
-  //   // Setting up our Renderer
-  //   renderer = new THREE.WebGLRenderer();
-  //   renderer.setSize( container.offsetWidth, container.offsetHeight );
-  //   container.appendChild( renderer.domElement );
-
-  //   // Making sure our renderer is always the right size
-  //   window.addEventListener( 'resize', onWindowResize , false );    
-   
-  //   leftHand = new ConnectedHand( controller );
-  //   leftHand.addToScene( scene );
-  //   rightHand = new ConnectedHand( controller );
-  //   rightHand.addToScene( scene );
-    
-  //   var geo = new THREE.IcosahedronGeometry( 2 , 1 );
-  //   var mat = new THREE.MeshNormalMaterial();
-  //   var joint = new THREE.Object3D();
-  //   var mesh = new THREE.Mesh(geo ,  mat );
-  //   joint.add( mesh );
-
-   
-  //   var connection = new THREE.Object3D();
-  //   var squisher = new THREE.Object3D();
-  //   var mat = new THREE.LineBasicMaterial();
-  //   mat.color.setHSL( .0 , .9 , .6 );
-
-  //   var line = createFlatCircle( -40 , 20 , 20 , mat );
-  //   squisher.add( line );
-  //   var line = createFlatCircle( -40 , 10 , 20 , mat );
-  //   squisher.add( line );
-    
-  //   var mat = new THREE.LineBasicMaterial();
-  //   mat.color.setHSL( .05 , .9 , .6 );
-  //   var line = createFlatCircle( -15 , 15 , 20 , mat );
-  //   squisher.add( line );
-  //   var line = createFlatCircle( -15 , 12 , 20 , mat );
-  //   squisher.add( line );
-
-
-  //   var mat = new THREE.LineBasicMaterial();
-  //   mat.color.setHSL( .1 , .9 , .6 );
-  //   var line = createFlatCircle( 5 , 10 , 20 , mat );
-  //   squisher.add( line );
-  //   var line = createFlatCircle( 5 , 8 , 20 , mat );
-  //   squisher.add( line );
-
-  //   var mat = new THREE.LineBasicMaterial();
-  //   mat.color.setHSL( .1 , .9 , .6 );
-  //   var line = createFlatCircle( 30 , 6 , 20 , mat );
-  //   squisher.add( line );
-  //   var line = createFlatCircle( 30 , 4 , 20 , mat );
-  //   squisher.add( line );
-
-  //   squisher.scale.y = .01;
-  //   squisher.scale.x = .12;
-  //   squisher.scale.z = .12;
-
-  //   connection.add( squisher );
-
-  //   palm = new THREE.Object3D();
-
-  //   for( var i = 0; i < 8; i++ ){
-  //     var mat = new THREE.LineBasicMaterial();
-  //     mat.color.setHSL( .4 + ( i * .01 ) , .9 , .6 );
-  //     var line = createFlatCircle( -1  , 4  , 20 , mat );
-  //     line.position.x = Math.cos(Math.PI * (i) / 4) * (6);
-  //     line.position.z = Math.sin(Math.PI * (i) / 4) * (6);
-  //     palm.add( line );
-  //   }
-
-  //   leftHand.createFingers( joint , connection );
-  //   leftHand.createPalm( joint , connection , palm );
-
-  //   rightHand.createFingers( joint , connection );
-  //   rightHand.createPalm( joint , connection , palm );
-
-  //   controller.connect();
-  // }
-
-  // function createCircleGeo( radius , count ){
-  //   var geometry = new THREE.Geometry();
-  //   for( var i = 0; i <= count; i++ ){
-  //     var t = ( i / count ) * Math.PI * 2;
-  //     v = new THREE.Vector3(
-  //       Math.cos( t ) * radius,
-  //       0,
-  //       Math.sin( t ) * radius
-  //     );
-  //     geometry.vertices.push( v );
-  //   }
-
-  //   return geometry;
-  // }
-
-  // function createFlatCircle( y , radius , count , mat ){
-  //   var geo = createCircleGeo( radius , count );
-  //   var line = new THREE.Line( geo , mat );
-  //   line.rotation.x = Math.PI //2;
-  //   line.position.y = y;
-  //   return line;
-  // }
-
-  // function animate(){
-  //   // Tells us which hand to update with
-  //   leftHand.update( 'left' );
-  //   rightHand.update( 'right' );
-
-  //   renderer.render( scene , camera );
-  //   requestAnimationFrame( animate );
-  // }
-
-  // // Resets the renderer to be the proper size
-  // function onWindowResize(){
-  //   camera.aspect = container.offsetWidth / container.offsetHeight;
-  //   camera.updateProjectionMatrix();
-
-  //   renderer.setSize( container.offsetWidth, container.offsetHeight );
-  // }
+  }
+});
+function animate() {
+  requestAnimationFrame( animate );
+  renderer.render( scene, camera );
+  controls.update();
+}
